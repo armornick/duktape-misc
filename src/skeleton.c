20af61361f254e32cc2b4441df57844a64ea5f6e
armornick <nickarnoeyts@gmail.com>
2015-08-22 19:55:36 +0200

    enhanced skeleton program

--- src/skeleton.c
+++ src/skeleton.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include "duktape.h"
 
+duk_ret_t pmain (duk_context *ctx); /* duktape main function */
+
 int main(int argc, char const *argv[]) {
 	duk_context *ctx = NULL;
 
@@ -12,9 +14,41 @@ int main(int argc, char const *argv[]) {
         exit(1);
     }
 
-    // program logic here
+    duk_push_c_function(ctx, pmain, 2);
+	duk_push_int(ctx, argc);
+	duk_push_pointer(ctx, argv);
+	if (duk_pcall(ctx, 2) != DUK_EXEC_SUCCESS) {
+		printf("%s: %s\n", argv[0], duk_to_string(ctx, -1));
+		exit(1);
+	}
 
     duk_destroy_heap(ctx);
+
+	return 0;
+}
+
+static void duk_push_argv (duk_context *ctx, int argc, char *argv[]) {
+	duk_idx_t arg_array_index; int i;
+
+	arg_array_index = duk_push_array(ctx);
+
+	for (i = 0; i < argc; i++) {
+		duk_push_string(ctx, argv[i]);
+		duk_put_prop_index(ctx, arg_array_index, i);
+	}
+
+	duk_put_global_string(ctx, "argv");
+}
+
+static duk_ret_t pmain (duk_context *ctx) {
+
+	/* register argv in program */
+	int argc; char** argv;
+	argc = duk_require_int(ctx, 0);
+	argv = duk_require_pointer(ctx, 1);
+	srduk_push_argv(ctx, argc, argv);
+
+	// program logic here
 
 	return 0;
 }
